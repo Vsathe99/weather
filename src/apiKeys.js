73d758b1cc62ca3225a2367d0533c49af7ea0253
vallabh sathe <117669872+Vsathe99@users.noreply.github.com>
2024-12-05 18:26:28 +0530

    complete

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "8157440a1936e2571661bc80bf38096a",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
